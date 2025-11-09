00ab496cf81ac9d51a3d256116715d0d32ea4c39
jonatascortes <joncortes.on@gmail.com>
2025-11-09 20:00:34 -0300

    feat: add a secret key to be used with Flask's ressource 'flash'

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from os import urandom
 
 
 def create_app():
     app = Flask(__name__)
 
-    from app.routes.routes import login_bp
-    app.register_blueprint(login_bp)
+    from app.routes.users_login import user_bp
+    app.register_blueprint(user_bp)
+    app.secret_key = urandom(32)
 
     return app
