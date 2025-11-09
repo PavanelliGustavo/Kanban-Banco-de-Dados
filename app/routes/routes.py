c7f729851228df03c4e7163c586f62d6349739f2
jonatascortes <joncortes.on@gmail.com>
2025-11-09 19:57:02 -0300

    feat(routes): rename login_bp to users_bp and capture login credentials to be sent to the backend

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, render_template, flash, session
 
 
-login_bp = Blueprint("login_bp", __name__)
 
+user_bp = Blueprint("user_bp", __name__)
 
-@login_bp.route('/')
+@user_bp.route('/')
 def login():
     return render_template('login.html')
+
+@user_bp.route('/login_request', methods=["POST"])
+def login_request():
+
+        parameters = request.form
+        user_type = parameters.get("user-type")
+
+        if user_type == "civil":
+            #logica para redirecionar o usuario civil
+            pass
+        else:
+            user = parameters.get("user_input")
+            password = parameters.get("password_input")
+            #logica para verificar usuario e senha
+            flash("ERRO: usuário ou senha incorretos", "info")
+            return redirect(url_for('user_bp.login'))
+             
+        
+
+
