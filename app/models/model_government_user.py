fb51462462cb72789312f9903e87b8422d53c261
Gustavo Pavanelli <69548993+PavanelliGustavo@users.noreply.github.com>
2025-11-27 23:29:00 -0300

    Update model_government_user.py

--- app/models/model_government_user.py
+++ app/models/model_government_user.py
@@ -1,31 +1,77 @@
-from app import db
-from .user import AuthentincatedUser
+from .model_user import AuthenticatedUser
+from app.db.database_connection import Database
 
-class Government(AuthentincatedUser):
-    # Government model inherits from AutenticatedUser (Joined Table Inheritance)
+class Government(AuthenticatedUser):
 
-    __tablename__ = 'Government'
+    TABLE_NAME = "Government"
+    MAX_DEPARTMENT_LENGTH: int = 100
+    
+    def __init__(self,
+                 name: str,
+                 email: str,
+                 department_name: str,
+                 password: str, # Recebe a senha em texto limpo para gerar o hash
+                 gov_id: int | None = None):
 
-    id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True) # Primary key and Foreign key to the 'User' table
+        password_hash = generate_password_hash(password)
+                     
+        super().__init__(user_id=gov_id if gov_id else 0,
+                         name=name,
+                         email=email,
+                         password_hash=password_hash)
 
-    # Atributos específicos do Governo
-    department_name = db.Column(db.String(100), nullable=False)
+        self.setDepartmentName(department_name)
+        
+        if not gov_id:
+            self.__addToDatabase()
 
-    # Relacionamentos de Saída -> Government é a "fonte" da FK
+    @classmethod
+    def getTableName(cls) -> str:
+        return cls.TABLE_NAME
 
-    # Government Signs Document (0,N)
-    signed_documents = db.relationship('Document', backref='signing_government', lazy='dynamic')
-    # A chave estrangeira será armazenada na tabela Document
-    ### RELAÇÃO 1:N. A FK TAMBÉM ESTÁ NO DOCUMENT
+    def __addToDatabase(self):
+        """ Insere o novo registro na tabela Government. """
+        
+        data = {
+            "department_name": self.getDepartmentName(),
+        }
+        
+        gov_id = self._create(data=data, returning="id")
+        self.__id = gov_id
 
-    # Government Register Public Work (1,N)
-    registered_works = db.relationship('PublicWork', backref='registrant_government', lazy='dynamic')
-    # lazy='dynamic' permite buscr eficientemente a lista de obras registradas
-    ### RELAÇÃO 1:N. A FK ESTÁ NO PUBLICWORK
+    def __updateInDatabase(self):
+        """ Atualiza os atributos na tabela Government. """
+        data = {
+            "department_name": self.getDepartmentName()
+        }
+        self._update(data)
+    
+    def setDepartmentName(self, department_name: str):
+        if not isinstance(department_name, str) or len(department_name.strip()) == 0:
+            raise ValueError("Department name must be a non-empty string.")
+        if len(department_name) > self.MAX_DEPARTMENT_LENGTH:
+            raise ValueError(f"Department name must be under {self.MAX_DEPARTMENT_LENGTH} characters.")
+        self.__department_name = department_name
+        self.__updateInDatabase()
 
-    # Para o JTI:
-    __mapper_args__ = {'polymorphic_identity': 'government'}
+    def getDepartmentName(self) -> str:
+        return self.__department_name
 
-    def __repr__(self):
-        return f'<Government {self.id} - {self.department_name}>'
+    @classmethod
+    def findByEmail(cls, email: str) -> 'Government' | None:
+        """ Busca e retorna uma instância de Government pelo email. """
+        columns = ["id", "name", "email", "password_hash", "department_name"]
+        
+        result = cls._selectByEmail(email, columns)
+        
+        if result:
+            (gov_id, name, email_val, password_hash, department_name) = result
+            return cls(gov_id=gov_id, 
+                       name=name, 
+                       email=email_val, 
+                       department_name=department_name, 
+                       password=password_hash)
+        return None
 
+    def delete(self):
+        Database.delete(_from=self.TABLE_NAME, where=f"id = {self.getId()}")
