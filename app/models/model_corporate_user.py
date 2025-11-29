8672941967b62024f90d37cb2f37365e8684d34e
jonatascortes <joncortes.on@gmail.com>
2025-11-29 13:33:17 -0300

    fix(models): fix imports and type hints

--- app/models/model_corporate_user.py
+++ app/models/model_corporate_user.py
@@ -1,12 +1,13 @@
-from .model_user import AuthenticatedUser
+from app.models.model_user import AuthenticatedUser
 from typing import Any
+
 
 class Corporate(AuthenticatedUser):
 
     TABLE_NAME = "Corporate"
     MAX_COMPANY_LENGTH: int = 100
     CNPJ_LENGTH: int = 14
-    
+
     def __init__(self,
                  company_name: str,
                  cnpj: str,
@@ -22,10 +23,10 @@ class Corporate(AuthenticatedUser):
         self.setCompanyName(company_name, update_db=False)
         self.setCnpj(cnpj, update_db=False)
         self.setFieldId(field_id, update_db=False)
-        
+
         if corp_id is None:
             self._addToDatabase()
-    
+
     def getData(self) -> dict:
         """ 
         Assume que a ordem das colunas no DB é: company_name, cnpj, field_id, email, password
@@ -38,22 +39,23 @@ class Corporate(AuthenticatedUser):
 
     @classmethod
     def _fromDatabaseRow(cls, row: tuple) -> 'Corporate':
-        
+
         # Assumindo a ordem id, cnpj, company_name, email, password, field_id
         corp_id, cnpj, company_name, email, password_hash, field_id = row
-        
-        return cls(corp_id=corp_id, 
-                   company_name=company_name, 
-                   cnpj=cnpj, 
-                   field_id=field_id, 
-                   email=email, 
+
+        return cls(corp_id=corp_id,
+                   company_name=company_name,
+                   cnpj=cnpj,
+                   field_id=field_id,
+                   email=email,
                    password=password_hash)
-    
+
     def setCompanyName(self, company_name: str, update_db: bool = True):
         if not isinstance(company_name, str) or len(company_name.strip()) == 0:
             raise ValueError("Company name must be a non-empty string.")
         if len(company_name) > self.MAX_COMPANY_LENGTH:
-            raise ValueError(f"Company name must be under {self.MAX_COMPANY_LENGTH} characters.")
+            raise ValueError(
+                f"Company name must be under {self.MAX_COMPANY_LENGTH} characters.")
         self.__company_name = company_name
         if self._id and update_db:
             self._updateInDatabase()
@@ -63,7 +65,8 @@ class Corporate(AuthenticatedUser):
 
     def setCnpj(self, cnpj: str, update_db: bool = True):
         if not isinstance(cnpj, str) or len(cnpj) != self.CNPJ_LENGTH:
-            raise ValueError(f"CNPJ must be a string of length {self.CNPJ_LENGTH}.")
+            raise ValueError(
+                f"CNPJ must be a string of length {self.CNPJ_LENGTH}.")
         self.__cnpj = cnpj
         if self._id and update_db:
             self._updateInDatabase()
@@ -77,6 +80,6 @@ class Corporate(AuthenticatedUser):
         self.__field_id = field_id
         if self._id and update_db:
             self._updateInDatabase()
-    
+
     def getFieldId(self) -> int:
         return self.__field_id
--- app/models/model_user.py
+++ app/models/model_user.py
@@ -1,18 +1,20 @@
-from .model_template import Model
+from app.db.database_connection import Database
+from app.models.model_template import Model
 from werkzeug.security import generate_password_hash, check_password_hash
 from abc import abstractmethod
-from typing import Any
+from typing import Any, Optional
+
 
 class AuthenticatedUser(Model):
-    
+
     MAX_EMAIL_LENGTH: int = 120
-    
+
     def __init__(self, email: str, password: str, user_id: int | None = None) -> None:
-        
-        self._id = user_id if user_id is not None else 0 
-        
+
+        self._id = user_id if user_id is not None else 0
+
         self.setEmail(email, update_db=False)
-        
+
         # Apenas verifica se a senha se parece com um hash
         if len(password) > 50 and '$' in password:
             self.__password_hash = password
@@ -23,7 +25,8 @@ class AuthenticatedUser(Model):
         if not isinstance(email, str) or len(email.strip()) == 0:
             raise ValueError("User email must be a non-empty string.")
         if len(email) > self.MAX_EMAIL_LENGTH:
-            raise ValueError(f"User email must be under {self.MAX_EMAIL_LENGTH} characters.")
+            raise ValueError(
+                f"User email must be under {self.MAX_EMAIL_LENGTH} characters.")
         self.__email = email
         if self._id and update_db:
             self._updateInDatabase()
@@ -35,14 +38,13 @@ class AuthenticatedUser(Model):
         self.__password_hash = generate_password_hash(password)
         if self._id and update_db:
             self._updateInDatabase()
-    
+
     def getPasswordHash(self) -> str:
         return self.__password_hash
 
-    
     def checkPassword(self, password: str) -> bool:
         return check_password_hash(self.getPasswordHash(), password)
-    
+
     def _getCommonData(self) -> dict:
         return {
             "email": self.getEmail(),
@@ -50,16 +52,16 @@ class AuthenticatedUser(Model):
         }
 
     @classmethod
-    def findByEmail(cls, email: str) -> 'AuthenticatedUser' | None:        
+    def findByEmail(cls, email: str) -> Optional['AuthenticatedUser']:
         results = Database.select(
             _from=cls.TABLE_NAME,
             where=f"email = '{email}'"
         )
-        
+
         if results:
             return cls._fromDatabaseRow(results[0])
         return None
-        
+
     @classmethod
     @abstractmethod
     def _fromDatabaseRow(cls, row: tuple) -> 'AuthenticatedUser':
