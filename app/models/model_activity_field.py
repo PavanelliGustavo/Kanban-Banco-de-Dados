f05cb678606d3f157e8b30ac89a131e2e3002440
jonatascortes <joncortes.on@gmail.com>
2025-11-30 17:56:17 -0300

    fix(ActivityFields): fix listCorporates and listPublicWorks

--- app/models/model_activity_field.py
+++ app/models/model_activity_field.py
@@ -3,22 +3,23 @@ from app.models.model_template import Model
 from app.models.model_public_work import PublicWork
 from app.models.model_corporate_user import Corporate
 
+
 class ActivityField(Model):
 
     TABLE_NAME = "tb_field_of_activity"
     MAX_NAME_LENGTH: int = 100
 
     def __init__(self,
-                 name:str) -> None:
+                 name: str) -> None:
 
         self.setName(name)
-    
+
     def setName(self, name: str):
         if len(name) == 0:
             raise ValueError("ActivityField name must not be null.")
         if len(name) > self.MAX_NAME_LENGTH:
             error = f"ActivityField name length must be under {self.MAX_NAME_LENGTH}."
-            raise(error)
+            raise (error)
         self.__name = name
 
     def getData(self) -> dict:
@@ -36,15 +37,15 @@ class ActivityField(Model):
         return [cls.instanceFromDatabaseRow(row) for row in rows]
 
     def listPublicWorks(self) -> list[PublicWork]:
-        tb_public_work = PublicWork.TABLE_NAME
-        ActivityField_match = f"id = {self.getId()}"
-        rows = Database.select(_from=tb_public_work,
-                               where=ActivityField_match)
+        tb_corporate_field_of_activity = "tb_corporate_field_of_activity"
+        field_of_activity_match = f"field_of_activity_id = {self.getId()}"
+        rows = Database.select(_from=tb_corporate_field_of_activity,
+                               where=field_of_activity_match)
         return [PublicWork.instanceFromDatabaseRow(row) for row in rows]
-    
+
     def listCorporates(self) -> list[Corporate]:
-        tb_corporates = Corporate.TABLE_NAME
-        ActivityField_match = f"id = {self.getId()}"
-        rows = Database.select(_from=tb_corporates,
-                               where=ActivityField_match)
+        tb_public_work_field_of_activity = "tb_public_work_field_of_activity"
+        field_of_activity_match = f"field_of_activity_id = {self.getId()}"
+        rows = Database.select(_from=tb_public_work_field_of_activity,
+                               where=field_of_activity_match)
         return [Corporate.instanceFromDatabaseRow(row) for row in rows]
