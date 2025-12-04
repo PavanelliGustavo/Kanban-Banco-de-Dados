112605db0d20684c2c5aa679648719935acf8c11
Gustavo Pavanelli <69548993+PavanelliGustavo@users.noreply.github.com>
2025-12-04 15:02:30 -0300

    Create 20251202_1710_view_concluded_public_works.sql

---
+++ app/sql/migrations/20251202_1710_view_concluded_public_works.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW vw_public_works_all_cards_in_last_column AS
+SELECT pw.*
+FROM tb_public_work pw
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM tb_card c
+    JOIN tb_column col ON col.id = c.column_id
+    WHERE c.public_work_id = pw.id
+      AND col.id <> (
+          SELECT id
+          FROM tb_column
+          WHERE public_work_id = pw.id
+          ORDER BY position DESC
+          LIMIT 1
+      )
+);
